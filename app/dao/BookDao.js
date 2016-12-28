d0a3cc38acb529f644ef7ebdff70afaa1828ae9f
Maxmengt <mengt2012@gmail.com>
2016-12-28 22:31:33 +0800

    add insertRecord function, which still have some bug

--- app/dao/BookDao.js
+++ app/dao/BookDao.js
@@ -1,6 +1,8 @@
-var connection = require('../db/connection');
-var queryWithArgs = connection.queryWithArgs;
-var query = connection.query;
+var InStockDao = require('./InStockDao.js'),
+    SoldDao = require('./SoldDao.js'),
+    connection = require('../db/connection'),
+    queryWithArgs = connection.queryWithArgs,
+    query = connection.query;
 
 /*
              输入           输出
@@ -62,10 +64,35 @@ exports.deleteOne = deleteOne;
 
 // 入库与出售
 var modify = function(book, callback) {
-    var sql = "UPDATE t_book SET ? WHERE Book_id = " + book.id;
+    var sql = "UPDATE t_book SET ? WHERE Book_id = " + book.id,
+        oldNum = { num: 0 },
+        obj;
+    selectOne(book.id, oldNum);
+    if( oldNum.num < book.currentNum ) {
+        obj = {
+            bookId:     book.id,
+            adminId:    book.adminId || [1, 2, 3][parseInt(Math.random() * 10) % 3],
+            instockNum: book.currentNum - oldNum.num,
+        };
+        // 调用DAO层接口
+        InStockDao.insert(obj, function() {
+            console.warn("添加入库记录成功");
+        });
+    }
+    else if( oldNum.num > book.currentNum ) {
+        obj = {
+            bookId:     book.id,
+            adminId:    book.adminId || [1, 2, 3][parseInt(Math.random() * 10) % 2],
+            soldNum:    oldNum.num - book.currentNum,
+        };
+        // 调用DAO层接口
+        SoldDao.insert(obj, function() {
+            console.warn("添加出售记录成功");
+        });
+    }
     console.log("sql:"+sql);
 
-    var obj = {
+    obj = {
         Book_id:        book.id,
         Book_name:      book.bookName,
         Type_id:        book.typeId,
@@ -98,7 +125,7 @@ var modify = function(book, callback) {
 exports.modify = modify;
 
 var selectAll = function(callback) {
-    var sql = "SELECT * FROM t_book,t_type WHERE t_book.Type_id = t_type.Type_id ORDER BY t_book.Book_id ASC";
+    var sql = "SELECT * FROM t_book,t_type WHERE t_book.Type_id = t_type.Type_id ORDER BY Book_id ASC";
     try {
         // 执行插入语句，成功返回success
         query(sql, function(err, rows) {
@@ -117,3 +144,21 @@ var selectAll = function(callback) {
 
 exports.selectAll = selectAll;
 
+var selectOne = function(id, temp){
+    var sql = "SELECT Current_num FROM t_book WHERE Book_id = ?";
+    try {
+        queryWithArgs(sql, id, function(err, rows) {
+            console.log("BookDaoSelectOneSuccess:" + rows);
+            if (err) {
+                console.error("BookDaoSelectOneError:" + err);
+            }
+            temp.num = parseInt(rows[0].Current_num);
+        });
+    } catch (er) {
+        // 错误则输出异常并输出错误
+        console.error("BookDaoSelectOneCatchError:" + er);
+        // callback(er);
+    }
+};
+
+exports.selectOne = selectOne;
